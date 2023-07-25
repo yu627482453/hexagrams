fca7248262cebec5a1a872e0bcd3b1e6bd34cab7
yu627482453 <yu627482453@gmail.com>
2023-07-25 23:09:58 +0800

    feat(hexagrams-starter): hexagrams-job-starter

---
+++ hexagrams-starter/hexagrams-job-starter/src/main/java/cn/bipher/hexagrams/job/properties/XxlJobProperties.java
@@ -0,0 +1,24 @@
+package cn.bipher.hexagrams.job.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+
+/**
+ * xxl-job配置
+ *
+ * @author Bipher
+ * @version 1
+ * @date 2023/7/25 23:05
+ */
+@Data
+@ConfigurationProperties(prefix = "xxl.job")
+public class XxlJobProperties {
+
+	@NestedConfigurationProperty
+	private XxlAdminProperties admin = new XxlAdminProperties();
+
+	@NestedConfigurationProperty
+	private XxlExecutorProperties executor = new XxlExecutorProperties();
+
+}
