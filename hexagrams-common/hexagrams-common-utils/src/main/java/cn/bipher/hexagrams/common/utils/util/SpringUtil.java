e760123fa9e1efae0b57d29e9e66de35b4e082ee
yu627482453 <yu627482453@gmail.com>
2023-07-26 18:04:25 +0800

    feat(hexagrams-starter): XSS starter

--- hexagrams-common/hexagrams-common-utils/src/main/java/cn/bipher/hexagrams/common/utils/util/SpringUtil.java
+++ hexagrams-common/hexagrams-common-utils/src/main/java/cn/bipher/hexagrams/common/utils/util/SpringUtil.java
@@ -1,36 +1,75 @@
 package cn.bipher.hexagrams.common.utils.util;
 
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationEvent;
 
 import java.util.Map;
 
 /**
- * 基于 Jackson 的 json 工具类
- *
  * @author Bipher
  * @version 1
  * @date 2023/2/21 9:30
  */
-public class SpringUtil implements ApplicationContextAware {
+@Slf4j
+public class SpringUtil implements ApplicationContextAware, DisposableBean {
 
     private static ApplicationContext applicationContext;
 
+    /**
+     * 实现ApplicationContextAware接口, 注入Context到静态变量中.
+     */
     @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    public void setApplicationContext(ApplicationContext applicationContext) {
         SpringUtil.applicationContext = applicationContext;
     }
 
-    public static <T> T getBean(String beanName) {
-        if(applicationContext.containsBean(beanName)){
-            return (T) applicationContext.getBean(beanName);
-        }else{
-            return null;
-        }
+    /**
+     * 从静态变量applicationContext中取得Bean, 自动转型为所赋值对象的类型.
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T getBean(String name) {
+        return (T) applicationContext.getBean(name);
     }
 
-    public static <T> Map<String, T> getBeansOfType(Class<T> baseType){
-        return applicationContext.getBeansOfType(baseType);
+    /**
+     * 从静态变量applicationContext中取得Bean, 自动转型为所赋值对象的类型.
+     */
+    public static <T> T getBean(Class<T> requiredType) {
+        return applicationContext.getBean(requiredType);
+    }
+
+    /**
+     * 清除SpringUtil中的ApplicationContext为Null.
+     */
+    public static void clearHolder() {
+        if (log.isDebugEnabled()) {
+            log.debug("清除SpringUtil中的ApplicationContext:" + applicationContext);
+        }
+        applicationContext = null;
+    }
+
+    /**
+     * 发布事件
+     * @param event
+     */
+    public static void publishEvent(ApplicationEvent event) {
+        if (applicationContext == null) {
+            return;
+        }
+        applicationContext.publishEvent(event);
+    }
+
+    /**
+     * 实现DisposableBean接口, 在Context关闭时清理静态变量.
+     */
+    @Override
+    @SneakyThrows
+    public void destroy() {
+        SpringUtil.clearHolder();
     }
 }
---
+++ hexagrams-starter/hexagrams-xss-starter/src/main/java/cn/bipher/hexagrams/xss/core/XssCleanIgnore.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2019-2029, Dreamlu 卢春梦 (596392912@qq.com & www.dreamlu.net).
+ * <p>
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE 3.0;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl.html
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.bipher.hexagrams.xss.core;
+
+import java.lang.annotation.*;
+
+/**
+ * 忽略 xss
+ *
+ * @author Bipher
+ * @version 1
+ * @date 2023/7/25 22:55
+ */
+@Target({ ElementType.TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface XssCleanIgnore {
+
+	/**
+	 * @return 需要跳过的字段列表
+	 */
+	String[] value() default {};
+
+}
